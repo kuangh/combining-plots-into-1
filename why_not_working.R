fff4ba859e25ba407619d0de19614e3a161154d8
kuangh <kuanghe@umich.edu>
2018-02-28 16:51:21 -0500

    "only part of line showing"

--- why_not_working.R
+++ why_not_working.R
@@ -1,3 +1,10 @@
-plot.new()
-lines(y=S_to_N_62bp, x=mLINE62bp_mouse$AnnealTemp, type = "b",cex=2, col ='6', pch=16)
+setwd("/Users/kuanghe/Desktop/R/ddPCR")
+
+
+plot(1, type="n", xlab="", ylab="", xlim=c(53:63), ylim=c(0,1000))
+
+
+points(x=0,y=1)
+
+lines(y=c(1:10), x=c(1:10), type = "b",cex=2, col ='6', pch=16)
 
